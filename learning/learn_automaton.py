18c5293e0eaed90ff55941c7a10e1da84db66921
Pierre-François Gimenez <pierre-francois.gimenez@centralesupelec.fr>
2024-09-10 16:16:36 +0200

    TADAM script

--- learning/learn_automaton.py
+++ learning/learn_automaton.py
@@ -13,7 +13,7 @@ def parse_TCP(input_string):
         Contains: flags, direction, payload type, iat, payload size
     """
     if "$" in input_string: return "$", [0,0]
-    regex_format2 = r'([A-Za-z]+)/([><])/([A-Za-z]+)/-?(\d+.\d+)/(\d+)'
+    regex_format2 = r'([A-Za-z]+)/([><])/([A-Za-z]+)/(-?\d+.\d+)/(\d+)'
     match = re.match(regex_format2, input_string)
     if match:
         return match.group(1) + "_" + match.group(2) + "_" + match.group(3), [int(float(match.group(4).replace(',', '.')) * 1e6), int(match.group(5))]
@@ -25,10 +25,10 @@ def parse_UDP(input_string):
         Contains: direction, payload type, iat, payload size
     """
     if "$" in input_string: return "$", [0,0]
-    regex_format2 = r'([A-Za-z]+)/([><])/([A-Za-z]+)/-?(\d+.\d+)/(\d+)'
+    regex_format2 = r'([><])/([A-Za-z]+)/(-?\d+.\d+)/(\d+)'
     match = re.match(regex_format2, input_string)
     if match:
-        return match.group(2) + "_" + match.group(3), [int(float(match.group(4).replace(',', '.')) * 1e6), int(match.group(5))]
+        return match.group(1) + "_" + match.group(2), [int(float(match.group(3).replace(',', '.')) * 1e6), int(match.group(4))]
     else:
         assert False, "Parsing error on "+input_string
 
@@ -37,10 +37,10 @@ def parse_ICMP(input_string):
         Contains: direction, iat
     """
     if "$" in input_string: return "$", [0]
-    regex_format2 = r'([A-Za-z]+)/([><])/([A-Za-z]+)/-?(\d+.\d+)/(\d+)'
+    regex_format2 = r'([><])/(-?\d+.\d+)'
     match = re.match(regex_format2, input_string)
     if match:
-        return match.group(2), [int(float(match.group(4).replace(',', '.')) * 1e6)]
+        return match.group(1), [int(float(match.group(2).replace(',', '.')) * 1e6)]
     else:
         assert False, "Parsing error on "+input_string
 
@@ -51,17 +51,21 @@ if __name__ == '__main__':
     group.add_argument('--select_dst_ports', type=int, nargs="+", help="Learn from these specific destination ports. Cannot be used with --ignore_dst_ports.")
     group.add_argument('--ignore_dst_ports', type=int, nargs="+", help="Learn from all ports except these. Cannot be used with --select_dst_ports.")
     parser.add_argument('--input', required=True, help="Select the input file. It must a FosR-netflow file.")
-    parser.add_argument('--output', required=True, help="Select the output file to create.")
+    parser.add_argument('--output', help="Select the output file to create.")
     parser.add_argument('--output_dot', help="Select the dot output file for visualization.")
+    parser.add_argument('--verbose', help="Increase TADAM’s verbosity.", action="store_true")
     args = parser.parse_args()
-    input_file = args.input
-    output_file = args.output
     protocol = args.proto
+
     select_dst_ports = args.select_dst_ports or []
     ignore_dst_ports = args.ignore_dst_ports or []
 
+    if args.output is None:
+        print("No output file specified: using \"automata.json\" by default")
+        args.output = "automata.json"
+
     try:
-        df = pd.read_csv(input_file,low_memory=False)
+        df = pd.read_csv(args.input,low_memory=False)
     except Exception as e:
         print("Input file cannot be opened:",e)
         exit()
@@ -103,26 +107,28 @@ if __name__ == '__main__':
                     search = Search.EXHAUSTIVE,
                     init = Init.STATE_SYMBOL)
 
-    l = exhaustive_search(options, tss_list=df, verbose=True, noise_model=noise_model)
-    print("Automaton successfully learned")
+    l = exhaustive_search(options, tss_list=df, verbose=args.verbose, noise_model=noise_model)
+    # print("Automaton successfully learned")
 
     d = l.ta.dict_export_ta()
     d["protocol"] = protocol
     metadata = {}
     metadata["select_dst_ports"] = args.select_dst_ports
-    metadata["input_file"] = os.path.split(input_file)[-1]
+    metadata["input_file"] = os.path.split(args.input)[-1]
     metadata["ignore_dst_ports"] = args.ignore_dst_ports
     metadata["creation_time"] = str(datetime.datetime.now())
     d["metadata"] = metadata
     try:
-        out_file = open(output_file, "w")
+        out_file = open(args.output, "w")
         json.dump(d, out_file, indent=4)
+        print("JSON file successfully created")
     except Exception as e:
         print("Error during json save:",e)
 
     if args.output_dot:
         try:
             l.ta.export_ta(args.output_dot, guard_as_distrib=True)
+            print("Dot file successfully created")
         except Exception as e:
             print("Error during dot save:",e)
 
