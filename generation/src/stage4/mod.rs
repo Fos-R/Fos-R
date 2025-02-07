1e37e35dcea51722072e0a8ee2b8a17418d2c410
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2025-02-07 10:48:25 +0100

    Bind l3 raw socket for the required protocol

--- generation/src/stage4/mod.rs
+++ generation/src/stage4/mod.rs
@@ -1,16 +1,47 @@
 #![allow(unused)]
 
+use std::collections::BinaryHeap;
+
+use pnet::transport::{
+    transport_channel, TransportChannelType, TransportReceiver, TransportSender,
+};
+use pnet_packet::ip::IpNextHeaderProtocols;
+
 use crate::*;
 
 pub struct Stage4 {
     interface: Ipv4Addr,
     proto: u8,
-    // TODO: add raw socket
+
+    raw_socket_tx: TransportSender,
+    raw_socket_rx: TransportReceiver,
 }
 
 impl Stage4 {
     pub fn new(interface: Ipv4Addr, proto: u8) -> Self {
-        Stage4 { interface, proto }
+        let ip_next_header_protocol = match proto {
+            TCP_PROTO => IpNextHeaderProtocols::Tcp,
+            UDP_PROTO => IpNextHeaderProtocols::Udp,
+            ICMP_PROTO => IpNextHeaderProtocols::Icmp,
+            _ => todo!("Unsupported protocol"),
+        };
+
+        let channel_type = TransportChannelType::Layer3(ip_next_header_protocol);
+
+        let (raw_socket_tx, raw_socket_rx) = match transport_channel(4096, channel_type) {
+            Ok((tx, rx)) => (tx, rx),
+            Err(e) => panic!(
+                "An error occurred when creating the transport channel: {}",
+                e
+            ),
+        };
+
+        Stage4 {
+            interface,
+            proto,
+            raw_socket_tx,
+            raw_socket_rx,
+        }
     }
 
     pub fn send(&self, packets: SeededData<Packets>) {
