bb4fecae6022e144cd1da3cf74feaec616201293
Pierre-François Gimenez <pierre-francois.gimenez@inria.fr>
2025-10-08 09:36:29 +0200

    GitLab CI saves failed deterministic test artifact

--- crates/fosr/tests/cli_tests.rs
+++ crates/fosr/tests/cli_tests.rs
@@ -5,12 +5,11 @@ use sha2::Sha256;
 use std::fs::File;
 use std::io;
 use std::process::Command;
-use tempfile::tempdir;
+use std::fs;
 
 #[test]
 fn deterministic_generation() -> Result<(), Box<dyn std::error::Error>> {
-    let dir = tempdir()?;
-    let file_path = dir.path().join("out.pcap");
+    let file_path = "deterministic-test.pcap";
 
     let mut cmd = Command::cargo_bin("fosr")?;
 
@@ -31,8 +30,10 @@ fn deterministic_generation() -> Result<(), Box<dyn std::error::Error>> {
     let mut sha256 = Sha256::new();
     io::copy(&mut file, &mut sha256)?;
     let hash = sha256.finalize();
-    Ok(assert_eq!(
+    assert_eq!(
         hash[..],
         hex!("8b4c7599bc223c2e4705a20e5d47f1c5fe357d773dd25a19b6a27281db163cb8")
-    ))
+    );
+    fs::remove_file(file_path)?;
+    Ok(())
 }
