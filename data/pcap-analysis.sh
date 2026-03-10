27e42c30a60098610e4616f414e4ff5ba5720937
Pierre-François Gimenez <pierre-francois.gimenez@inria.fr>
2026-03-10 23:09:50 +0100

    Add pcap-analysis.sh for CI

---
+++ data/pcap-analysis.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [ -f "data.pcap" ]; then
+    echo "Zeek analysis"
+    zeek -C -r data.pcap
+    echo "TShark analysis"
+    tshark -r data.pcap -T fields -E header=y -E separator=, -E occurrence=f -e ip.len -e ip.ttl -e ip.src -e ip.dst -e ip.proto "eth.type == 0x0800" > ip.csv
+    tshark -r data.pcap -T fields -E header=y -E separator=, -E occurrence=f -e tcp.flags "ip.proto == 6" > tcp.csv
+    tshark -r data.pcap -z expert,warn -q > expert_info.txt
+    echo "Suricata analysis"
+    suricata -c /etc/suricata/suricata.yaml -r data.pcap > suricata.txt
+else
+    echo "Call this script from a directory containing data.pcap"
+fi
